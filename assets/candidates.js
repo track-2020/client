b5d288c302636bf872ae2912012b75b8a338fe0f
UncleJerry <jared.myhrberg@gmail.com>
2020-02-28 11:42:31 -0800

    rough landing page outlined

---
+++ assets/candidates.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    name: 'Bernie Sanders',
+    image: 'https://imgix.bustle.com/2016/10/2/1-9fcb23f5-021d-4d3d-b093-5f19b0909ab0.jpg?w=960&h=540&fit=crop&crop=faces&auto=format&q=70',
+    points: 38
+  },
+  {
+    name: 'Pete Buttigieg',
+    image: 'https://thenypost.files.wordpress.com/2019/11/mayor-pete-46.jpg?quality=80&strip=all&w=618&h=410&crop=1',
+    points: 27
+  },
+  {
+    name: 'Joe Biden',
+    image: 'https://img.huffingtonpost.com/asset/5b9e55f12000002d00fe50a6.jpeg?ops=scalefit_720_noupscale',
+    points: 20
+  },
+  {
+    name: 'Elizabeth Warren',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRV3u4H1TapTA_WxlS-28quKJcO-BSEQRSnSVt-cp6fAfu1Aek5',
+    points: 27
+  },
+  {
+    name: 'Amy Klobuchar',
+    image: 'https://cdn.cnn.com/cnnnext/dam/assets/190610182606-03-amy-klobuchar-gallery-super-169.jpeg',
+    points: 9
+  },
+  {
+    name: 'Elizabeth Warren',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRV3u4H1TapTA_WxlS-28quKJcO-BSEQRSnSVt-cp6fAfu1Aek5',
+    points: 27
+  },
+  {
+    name: 'Amy Klobuchar',
+    image: 'https://cdn.cnn.com/cnnnext/dam/assets/190610182606-03-amy-klobuchar-gallery-super-169.jpeg',
+    points: 9
+  }
+];
