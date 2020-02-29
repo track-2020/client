7e30636cd63f5456d874fe8ba458ad680651bf34
Ben Parisot <me@benparisot.com>
2020-02-28 16:05:03 -0800

    write click handle function on modal

--- src/components/ScoreModal.js
+++ src/components/ScoreModal.js
@@ -1,13 +1,25 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import candies from '../../assets/candidates';
 // import './ScoreModal.scss';
 
-const ScoreModal = ({ show, close }) => {
+const ScoreModal = ({ show, close, candidate }) => {
+
+    const handleScoreAssignment = ({ target }) => {
+        console.log(target.name, 'was clicked');
+    };
+
     console.log('show', show);
     const hideShowStyle = show ? { display: 'block' } : { display: 'none' };
 
     return (
         <div style={hideShowStyle}>
             <h1 className='test'>Score Modal</h1>
+            <img name="environment" onClick={handleScoreAssignment} width="50px" src="https://image.flaticon.com/icons/svg/525/525903.svg"></img>
+            <img name="lgbtq" onClick={handleScoreAssignment} width="50px" src="https://image.flaticon.com/icons/svg/2284/2284888.svg"></img>
+            <img name="voting" onClick={handleScoreAssignment} width="50px" src="https://image.flaticon.com/icons/svg/2633/2633809.svg"></img>
+            <img name="general" onClick={handleScoreAssignment} width="50px" src="https://image.flaticon.com/icons/svg/1665/1665578.svg"></img>
+            <img width="50px" src={candies[0].image}></img>
             <button onClick={close}>Close Modal</button>
         </div>
     );
